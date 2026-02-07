7e95c89e1d97a001335c39c03df0084839a784ea
Lauren Snyder <lauren@topwebworks.com>
2026-02-07 10:50:51 -0700

    added 2 methods to week 1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,19 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Read in the number
+        // Starting at '1', multiply the number incremently until the length is hit
+        // multiplesOf(7, 5) should return {7, 14, 21, 28, 35}
 
-        return []; // replace this return statement with your own
+        List<double> multiples = new List<double>();
+        for (var i = 1; i <= length; ++i) {
+            var multiple = number * i;
+
+            // add the multiple to the array
+            multiples.Add(multiple);            
+        }     
+
+        return multiples.ToArray();
     }
 
     /// <summary>
@@ -25,9 +32,35 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Read in the list and the amount
+        // Take the last 'amount' of items and move them to the front of the list
+        // RotateListRight(List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9}, 3) should modify the list to be List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}
+
+        var count = data.Count;
+        if (amount < count) 
+        {
+            // If the amount is less than the count, perform the rotation normally
+            var rotated = new List<int>(data);
+            for (var i = 0; i < count; ++i) {
+                var rotatedIndex = (i + amount) % count;
+                rotated[rotatedIndex] = data[i];
+            }
+
+            for (var i = 0; i < count; ++i) {
+                data[i] = rotated[i];
+            }
+        } 
+        else if (amount == count) 
+        {
+            // If the amount is the same as the count, then the list will be the same after rotation
+            // No need to do anything
+            return;
+        } 
+        else if (amount > count) 
+        {
+            // If the amount is greater than the count, then we can rotate by the amount mod the count
+            RotateListRight(data, amount % count);
+        }
+
     }
 }
